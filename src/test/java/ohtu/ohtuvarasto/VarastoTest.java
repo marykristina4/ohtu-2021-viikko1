1117a2c65a84c0799ef8ccaafdcdffdb3d696968
marykristina4 <mary.ehrstedt@helsinki.fi>
2021-01-22 10:02:21 +0200

    korjaus taas testin jälkeen

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -30,7 +30,7 @@ public class VarastoTest {
 
     @Test
     public void konstruktoriLuoTyhjanVaraston() {
-        assertEquals(2, varasto.getSaldo(), vertailuTarkkuus);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
     }
 
     @Test
